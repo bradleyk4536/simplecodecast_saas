ba4a7f099dcb08a346336266ca0a5b40c0b13bac
Kenneth Bradley <k_bnet570@verizon.net>
2016-04-11 07:34:47 -0400

    Added contact route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
+	
+	resources :contacts
 	get '/about' => 'pages#about'
+  # You can have the root of your site routed with "root"
+	
 	root 'pages#home'
 
   # Example of regular route:
