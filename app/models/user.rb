665305b2ac52e9749e0b5ee05755e336a134ef4a
Kenneth Bradley <k_bnet570@verizon.net>
2016-04-13 11:34:55 -0400

    Added plan button to home page

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 	
+#	associate to plan table
 	belongs_to :plan
 end
