f0960dece91bd28e06220448cd93400ace6c00e2
Kenneth Bradley <k_bnet570@verizon.net>
2016-04-15 10:26:44 -0400

    Added controller and nested routes to application

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 	
+	resources :users do
+		resource :profile
+	end
+
 	resources :contacts
 	get 'about', to: 'pages#about'
   # You can have the root of your site routed with "root"
